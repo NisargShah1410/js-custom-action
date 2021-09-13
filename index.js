97faa96488e8e110eb55975582c2ad258a5a8f31
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-09-13 09:24:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  //const lang = core.getInput('language-name');
-  var lang = "dotnet"
+  const lang = core.getInput('language-name');
   console.log('Language selected is ${lang}');
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
