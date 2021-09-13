b48d3cef9965d1b08157efea66c4173079874a8e
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-09-13 09:41:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const lang = core.getInput('language-name');
-  console.log('Language selected is ${lang}');
+  const nameToGreet = core.getInput('language-name');
+  console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
