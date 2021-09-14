efb044c8af908147165c9b01160664a4fed10817
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-09-14 08:39:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,6 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const { exec } = require("child_process");
-
-  exec("ls -la", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
   const lang = core.getInput('language-name');
   console.log(`Language selected is ${lang}`);
   const time = (new Date()).toTimeString();
