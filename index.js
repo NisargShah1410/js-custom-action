847674dc1679c775b017a86f565396665a44bfd7
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-09-20 13:46:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const { exec } = require("child_process");
 
-try {
-  const lang = core.getInput('language-name');
-  console.log(`Language selected is ${lang}`);
-  const com = `${lang} build`.toString();
-  core.setOutput("com", com);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
