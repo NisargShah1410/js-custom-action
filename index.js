ad1f52c5b2be66686b5758f6b2a713d546e64bdb
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-09-20 13:52:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const { exec } = require("child_process");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-exec("ls -la", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
